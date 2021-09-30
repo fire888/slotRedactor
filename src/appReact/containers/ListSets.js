1f371bff3850ec4af0c2a7c336cd58f3cd1eb649
vasilii <porokh.vs@gmail.com>
2021-10-01 01:21:15 +0300

    pr

--- src/appReact/containers/ListSets.js
+++ src/appReact/containers/ListSets.js
@@ -6,18 +6,25 @@ import { sendResponse } from "../../toServerApi/requests";
 
 export function ListSets(props) {
     const [loadedRes, showList] = useState(null)
+    const [openedId, changeOpenedId] = useState(null)
 
     const userRole = localStorage.getItem('userRole')
 
     const prepareList = data => {
         console.log(data.list)
-        const arr = data.list.map(item =>
-            <AppButton
-                classNameCustom={'long'}
-                key={item.id}
-                val={item.name}
-                callBackClick = {() => props.callBackClick(props.type, item)}
-            />)
+        const arr = data.list.map(item => 
+            <div>
+                <AppButton
+                    classNameCustom={'long'}
+                    key={item.id}
+                    val={item.name}
+                    callBackClick = {() => { 
+                        //props.callBackClick(props.type, item)
+                        //console.log(item.id, openedId, item.id === openedId ? null : item.id)
+                        changeOpenedId(item.id) 
+                    }}/>
+                {openedId}    
+            </div>)
 
         showList(arr)
     }
